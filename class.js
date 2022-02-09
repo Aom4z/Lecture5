646cde19b90efbf16fcd4c20f6527483c01fc18e
Aom4z <6306021630056@fitm.kmutnb.ac.th>
2022-02-09 11:06:45 +0700

    update

---
+++ class.js
@@ -0,0 +1,12 @@
+class Song {
+    constructor(){
+        this.title;
+        this.author;
+    }
+    play(){
+        console.log("Song laying!")
+    }
+}
+
+const mySong = new Song();
+mySong.play();
